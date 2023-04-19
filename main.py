d8efb37db1ddc7769d05472ef513a743d1194c9b
ctinsman <ctinsman@thegraysonschool.org>
2023-04-19 08:43:17 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+class BinarySearchTree:
+    # This is a Node class that is internal to the BinarySearchTree class.
+    class __Node:
+        def __init__(self, val, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+        def getVal(self):
+            return self.val
+
+        def setVal(self, newVal):
+            self.val = newVal
+
+        def getLeft(self):
+            return self.left
+
+        def getRight(self):
+            return self.right
+
+        def setLeft(self, newLeft):
+            self.left = newLeft
+
+        def setRight(self, newRight):
+            self.right = newRight
+
+    def __init__(self):
+        self.root = None
+
+    # Insert something into the tree.
+    def insert(self, val):
+        pass
+
+    # Check if a value is in the tree.
+    def contains(self, val):
+        pass
+
+    # Return all the items in the tree as a sorted list.
+    def toList(self):
+        pass
+
+def main():
+    s = input("Enter a list of numbers, seperated by spaces: ")
+    lst = s.split()
+
+    tree = BinarySearchTree()
+    for x in lst:
+        tree.insert(float(x))
+
+    print(tree.toList())
+
+
+if __name__ == "__main__":
+    main()
